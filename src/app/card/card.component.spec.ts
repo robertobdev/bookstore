be89be3d27b8468a15a51898a4440e68fced864f
Roberto <robertojf95@gmail.com>
2019-11-02 16:40:34 -0300

    adding card test

--- src/app/card/card.component.spec.ts
+++ src/app/card/card.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { By } from '@angular/platform-browser';
+import { AppModule } from '../app.module';
+import { CustomMaterialModule } from '../shared/custom-material.module';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -8,18 +11,53 @@ describe('CardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [CardComponent],
+      imports: [CustomMaterialModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    component.book = {
+      id: 1,
+      price: 35.00,
+      title: 'O Senhor dos Anéis: A Sociedade do Anel',
+      description: '',
+      author: 'J.R.R. Tolkien',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81SWBRKfExL.jpg'
+    };
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show image, title, price ', () => {
+    const arrage = {
+      expect: {
+        id: 1,
+        price: '35',
+        title: 'O Senhor dos Anéis: A Sociedade do Anel',
+        description: '',
+        author: 'J.R.R. Tolkien',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/81SWBRKfExL.jpg'
+      }
+    };
+
+    const title = fixture.debugElement.query(By.css('h4')).nativeElement;
+    const price = fixture.debugElement.query(By.css('h5')).nativeElement;
+    const image = fixture.debugElement.query(By.css('img')).nativeElement;
+    fixture.detectChanges();
+
+    expect(title.innerText).toEqual(arrage.expect.title);
+    expect(price.innerText).toEqual(arrage.expect.price);
+    expect(image.src).toContain(arrage.expect.image);
+  });
+
+  it('should adding book to storageSession', () => {
+    sessionStorage.clear();
+    component.addingToCart();
+    const cart = JSON.parse(sessionStorage.getItem('cart'));
+    fixture.detectChanges();
+    expect(cart.length).toEqual(1);
   });
 });
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() book;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  addingToCart() {
+    let cart: any = JSON.parse(sessionStorage.getItem('cart'));
+    if (!cart) {
+      cart = [];
+    }
+    sessionStorage.setItem('cart', JSON.stringify([...cart, this.book]));
+  }
 }
