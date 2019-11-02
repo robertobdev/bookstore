ec8928d97f7fb1440584f36e17b78509062d972a
Roberto <robertojf95@gmail.com>
2019-11-02 14:02:54 -0300

    move folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { CustomMaterialModule } from './custom-material.module';
+import { CustomMaterialModule } from './shared/custom-material.module';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
