23a3e465e16647f380430217ff9f25d59913daf5
Roberto <robertojf95@gmail.com>
2019-11-02 13:06:52 -0300

    create header component and first test

---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should show a title and button', () => {
+    const title = fixture.debugElement.query(By.css('h1')).nativeElement;
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+
+    expect(title.innerText).toEqual('BookStore');
+    expect(button).toBeTruthy();
+  });
+
+});
