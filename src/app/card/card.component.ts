a5067279bbcd5fa31ad29cc4dd7bf14b1f32ee23
Roberto <robertojf95@gmail.com>
2019-11-04 19:50:24 -0300

    change style and add output

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CardComponent implements OnInit {
 
   @Input() book;
+  @Output() addToCart: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
@@ -23,10 +24,6 @@ export class CardComponent implements OnInit {
   }
 
   addingToCart() {
-    let cart: any = JSON.parse(sessionStorage.getItem('cart'));
-    if (!cart) {
-      cart = [];
-    }
-    sessionStorage.setItem('cart', JSON.stringify([...cart, this.book]));
+    this.addToCart.emit(this.book);
   }
 }
