a4cff0b016ec4f7402c2a07c20f29dfe6fcde383
Roberto <robertojf95@gmail.com>
2019-11-04 20:42:14 -0300

    adding badge

--- src/app/shared/custom-material.module.ts
+++ src/app/shared/custom-material.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 
 import {
   MatButtonModule, MatSelectModule, MatFormFieldModule, MatInputModule,
-  MatIconModule, MatOptionModule, MatMenuModule, MatDialogModule, MatSnackBarModule
+  MatIconModule, MatOptionModule, MatMenuModule, MatDialogModule, MatSnackBarModule,
+  MatBadgeModule
 } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule, MatSelectModule, MatFormFieldModule, MatInputModule, MatMenuModule,
-    MatIconModule, MatOptionModule, MatDialogModule, MatSnackBarModule,
+    MatIconModule, MatOptionModule, MatDialogModule, MatSnackBarModule, MatBadgeModule,
   ],
   exports: [
     MatButtonModule, MatSelectModule, MatFormFieldModule, MatInputModule, MatMenuModule,
-    MatIconModule, MatOptionModule, MatDialogModule, MatSnackBarModule,
+    MatIconModule, MatOptionModule, MatDialogModule, MatSnackBarModule, MatBadgeModule,
   ],
 })
 export class CustomMaterialModule { }
