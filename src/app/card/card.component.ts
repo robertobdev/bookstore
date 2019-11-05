f44923f28b55852f312c2b5205f5aea9e7deba4d
Roberto <robertojf95@gmail.com>
2019-11-04 21:56:57 -0300

    remove cart model

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,23 +8,13 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class CardComponent implements OnInit {
 
   @Input() book;
-  @Input() isCart;
+  @Input() isCart = false;
   @Output() addToCart: EventEmitter<any> = new EventEmitter();
   @Output() removeBook: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {
-    this.isCart = false;
-    this.book = {
-      id: 1,
-      price: 35.00,
-      title: 'O Senhor dos Anéis: A Sociedade do Anel',
-      description: '',
-      author: 'J.R.R. Tolkien',
-      image: 'https://images-na.ssl-images-amazon.com/images/I/81SWBRKfExL.jpg'
-    };
-  }
+  ngOnInit() {}
 
   addingToCart() {
     this.addToCart.emit(this.book);
