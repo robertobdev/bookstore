f317af4dafd09c675d50cf5d9c0b58cdda3a1e74
Roberto <robertojf95@gmail.com>
2019-11-04 21:55:24 -0300

    remove cart

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -14,17 +14,7 @@ export class CardComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-    this.isCart = false;
-    this.book = {
-      id: 1,
-      price: 35.00,
-      title: 'O Senhor dos Anéis: A Sociedade do Anel',
-      description: '',
-      author: 'J.R.R. Tolkien',
-      image: 'https://images-na.ssl-images-amazon.com/images/I/81SWBRKfExL.jpg'
-    };
-  }
+  ngOnInit() {}
 
   addingToCart() {
     this.addToCart.emit(this.book);
