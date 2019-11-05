abb1d27fcf896fa0c44bce9c60acab1521b2c5b8
Roberto <robertojf95@gmail.com>
2019-11-04 21:42:51 -0300

    adding cart and card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListCardsComponent } from './list-cards/list-cards.component';
+import { CartComponent } from './cart/cart.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: ListCardsComponent
+  },
+  {
+    path: 'carrinho',
+    component: CartComponent
   },
 ];
 
